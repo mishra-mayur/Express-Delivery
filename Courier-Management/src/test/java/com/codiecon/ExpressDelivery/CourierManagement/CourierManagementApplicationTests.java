990070127383b4cc326f7ee7ff95f7c0abad4041
MAYUR MISHRA <mayurmishra14@gmail.com>
2019-12-13 15:24:49 +0530

    initial setup

---
+++ Courier-Management/src/test/java/com/codiecon/ExpressDelivery/CourierManagement/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codiecon.ExpressDelivery.CourierManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
